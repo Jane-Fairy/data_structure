cc157092fe30607d19cc548f92d2153f5a28104c
Adulatian <jin.tian984@outlook.com>
2023-09-04 10:40:46 +0800

    insertion sort.

--- dayone/sort_test.go
+++ dayone/sort_test.go
@@ -5,16 +5,19 @@ import (
 	"testing"
 )
 
-func TestSelectorSort(t *testing.T) {
+func TestSort(t *testing.T) {
 	arr := []int{10, 2, 4, 7, 7, 9, 2, 90}
 
-	sign := 1
+	sign := 2
 
 	switch sign {
+
 	case 0:
 		SelectorSort(arr)
 	case 1:
 		BubbleSort(arr)
+	case 2:
+		insertionSort(arr)
 	}
 
 	fmt.Println(arr)
